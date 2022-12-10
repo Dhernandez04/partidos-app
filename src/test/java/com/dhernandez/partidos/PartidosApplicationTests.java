bd87015058c1a386b81dd8ae3aa6872edda00606
Dhernandez04 <deimercode@gmail.com>
2022-12-09 21:19:04 -0500

    Agregando proyecto

---
+++ src/test/java/com/dhernandez/partidos/PartidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhernandez.partidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
